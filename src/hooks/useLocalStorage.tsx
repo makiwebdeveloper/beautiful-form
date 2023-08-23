def89f0e3c5db0307925b85d5bc7e38271f6d1d2
makiwebdeveloper <makiwebdeveloper@gmail.com>
2023-08-23 19:55:39 +0200

    fix

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -8,7 +8,10 @@ export default function useLocalStorage<T>(
 ): [T, SetValue<T>] {
   const [value, setValue] = useState<T>(() => {
     const storedValue = localStorage.getItem(key);
-    return storedValue ? JSON.parse(storedValue) : initialValue;
+
+    return storedValue || storedValue === "undefined"
+      ? JSON.parse(storedValue)
+      : initialValue;
   });
 
   useEffect(() => {
